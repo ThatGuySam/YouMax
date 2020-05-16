59ef3b9af20c513ebbd692b047889cc2fac8fe4b
Sam Carlton <sam@sam.lc>
2020-05-15 19:40:27 -0500

    Configure PurgeCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,27 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: [
+      './src/**/*.vue',
+      './src/**/*.js',
+      './src/**/*.html',
+    ],
+    whitelist: [
+      'body',
+      'html',
+      'img',
+      'a',
+      'g-image',
+      'g-image--lazy',
+      'g-image--loaded',
+    ],
+    extractors: [
+        {
+            extractor: content => content.match(/[A-z0-9-:\\/]+/g),
+            extensions: ['vue', 'js', 'jsx', 'md', 'html', 'pug'],
+        },
+    ],
+  },
   theme: {
     extend: {},
   },
