00782c6ecd90fceb79865abbae8698988129fba6
Sam Carlton <sam@sam.lc>
2020-05-15 19:55:06 -0500

    Move purge config into gridsome config

--- gridsome.config.js
+++ gridsome.config.js
@@ -9,6 +9,28 @@ module.exports = {
   plugins: [
     {
       use: 'gridsome-plugin-tailwindcss',
+      purgeConfig: {
+        content: [
+          './src/**/*.vue',
+          './src/**/*.js',
+          './src/**/*.html',
+        ],
+        whitelist: [
+          'body',
+          'html',
+          'img',
+          'a',
+          'g-image',
+          'g-image--lazy',
+          'g-image--loaded',
+        ],
+        extractors: [
+          {
+              extractor: content => content.match(/[A-z0-9-:\\/]+/g),
+              extensions: ['vue', 'js', 'jsx', 'md', 'html', 'pug'],
+          },
+        ],
+      },
     }
   ]
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,4 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    content: [
-      './src/**/*.vue',
-      './src/**/*.js',
-      './src/**/*.html',
-    ],
-    whitelist: [
-      'body',
-      'html',
-      'img',
-      'a',
-      'g-image',
-      'g-image--lazy',
-      'g-image--loaded',
-    ],
-    extractors: [
-        {
-            extractor: content => content.match(/[A-z0-9-:\\/]+/g),
-            extensions: ['vue', 'js', 'jsx', 'md', 'html', 'pug'],
-        },
-    ],
-  },
   theme: {
     extend: {},
   },
